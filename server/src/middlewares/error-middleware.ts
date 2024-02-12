e4ac412f9761230ec992402501a1e07d26a884d7
Aleksandar Atanasov <aleks930819@gmail.com>
2024-02-12 19:42:00 +0200

    Add global error middleware handler

---
+++ server/src/middlewares/error-middleware.ts
@@ -0,0 +1,36 @@
+/**
+ * Middleware function to handle 404 Not Found errors.
+ * @param {Request} req - The request object.
+ * @param {Response} res - The response object.
+ * @param {NextFunction} next - The next middleware function.
+ */
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+/**
+ * Express error handling middleware.
+ *
+ * @param {Error} err - The error object.
+ * @param {Request} req - The Express request object.
+ * @param {Response} res - The Express response object.
+ * @param {NextFunction} next - The Express next function.
+ */
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  });
+};
+
+const errorMiddleware = {
+  notFound,
+  errorHandler
+};
+
+export default errorMiddleware;
